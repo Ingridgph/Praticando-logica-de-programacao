c14c189fb440fc627702703631c479d4862ff4c8
Ingrid Gomes <ingridgomespereira98@gmail.com>
2024-12-13 09:52:54 -0300

    atividades adicionais2

--- atividades-adicionais/atividadesadicionais.js
+++ atividades-adicionais/atividadesadicionais.js
@@ -22,3 +22,24 @@ function ordenarNumeros(num1, num2, num3) {
 let resultado = ordenarNumeros(15, 8, 42);
 console.log("Números ordenados:", resultado);
 }
+
+function atividade2(){}
+/*Crie um formulário simples com campos de entrada e utilize JavaScript para recuperar os valores digitados nos campos usando a propriedade value. Exiba os valores no console e na tela.
+
+Tenha um elemento HTML na página (por exemplo, um parágrafo) e utilize JavaScript para modificar seu conteúdo usando a propriedade textContent.
+
+Faça a soma de duas variáveis numéricas e apresente no console com uma mensagem amigável em Template String.
+
+Receba, no mínimo, duas sentenças como uma string e utilize split() para quebrá-la em frases menores com base em um ponto e vírgula como delimitador.
+
+Receba uma string contendo números separados por vírgulas. Utilize split() para separar os números e exibi-los no console.*/
+function botao(){
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let genero = document.getElementById('genero').value;
+    let mensagem = document.getElementById('mensagem').value;
+    alert (nome);
+    alert (email);
+    alert (genero);
+    alert (mensagem);
+}
--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,27 +1,31 @@
-function adicionar(){  
+function adicionar(){
+    
     //recuperar produtos/quantidade/preço
     let produto = document.getElementById('produto').value;
     //split é o separador sendo o - e o R$ o separando
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = parseFloat(produto.split('R$')[1]);
-    let quantidade=document.getElementById('quantidade');
-    let preco=parseFloat(valorUnitario*quantidade.value);
+    let quantidade=document.getElementById('quantidade').value;
+    let preco=parseFloat(valorUnitario*quantidade);
     let valorTotal = 0;
+    if(quantidade>0){
     valorTotal +=preco;
     //pegue o elemento pelo id
     let listaProdutos=document.getElementById('lista-produtos');
     //cria uma string da section
     let novoItem=`
     <section class="carrinho__produtos__produto">
-      <span class="texto-azul">${quantidade.value}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+      <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
     </section>`;
     //na variavel lista produto dentro do html e some com o novo intem
     listaProdutos.innerHTML += novoItem;
 
     //pegue o elemento pelo id
     let valorTotalTexto=document.getElementById('valor-total');
-    valorTotalTexto.innerHTML=`R$${valorTotal}`;
-   
+    valorTotalTexto.innerHTML=`R$${valorTotal}`;}
+    else{
+        alert('A quantidade não pode ser igual ou menor que 0');
+    }
 }
 
 function limpar(){
